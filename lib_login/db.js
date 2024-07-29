464847d33aa33f902f32f3293ec84b9da6481c1b
doopyo85 <doopyo85@gmail.com>
2024-07-29 11:25:21 +0900

    db_log

--- lib_login/db.js
+++ lib_login/db.js
@@ -3,8 +3,18 @@ var db = mysql.createConnection({
     host: 'database-1.cte6iy2wqmcn.ap-northeast-2.rds.amazonaws.com',
     user: 'codingpioneer',
     password: 'fq84cod3',
-    database: 'educodingnplay'
+    database: 'educodingnplay',
+    connectTimeout: 10000,  // 추가
+    acquireTimeout: 10000,  // 추가
+    connectionLimit: 5,     // 추가
+    queueLimit: 30          // 추가
 });
-db.connect();
+db.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + db.threadId);
+});
 
 module.exports = db;
