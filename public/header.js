48d77f7054e66e6c96689f0d135323a1f90b25a0
doopyo85 <doopyo85@gmail.com>
2024-07-30 23:51:50 +0900

    로그아웃4

--- public/header.js
+++ public/header.js
@@ -1,26 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 세션 유지
-    fetch('/get-user')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("userEmail").innerText = data.email || "로그인 정보 미확인";
-        })
-        .catch(error => {
-            console.error('Error fetching user data:', error);
-            document.getElementById("userEmail").innerText = "로그인 정보 미확인";
-        });
+    // header.html이 로드된 후 실행
+    $("#header-placeholder").load("/public/header.html", function() {
+        // 세션 유지
+        fetch('/get-user')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("userEmail").innerText = data.email || "로그인 정보 미확인";
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+                document.getElementById("userEmail").innerText = "로그인 정보 미확인";
+            });
 
-    // 로그아웃 버튼 클릭 이벤트
-    const logoutButton = document.getElementById("logoutButton");
-    if (logoutButton) {
-        logoutButton.addEventListener("click", function() {
-            fetch('/auth/logout', { method: 'GET' })
-                .then(() => {
-                    window.location.href = '/auth/login'; // 로그아웃 후 로그인 페이지로 리디렉션
-                })
-                .catch(error => console.error('Error logging out:', error));
-        });
-    } else {
-        console.error("Logout button not found");
-    }
+        // 로그아웃 버튼 클릭 이벤트
+        const logoutButton = document.getElementById("logoutButton");
+        if (logoutButton) {
+            logoutButton.addEventListener("click", function() {
+                fetch('/auth/logout', { method: 'GET' })
+                    .then(() => {
+                        window.location.href = '/auth/login'; // 로그아웃 후 로그인 페이지로 리디렉션
+                    })
+                    .catch(error => console.error('Error logging out:', error));
+            });
+        } else {
+            console.error("Logout button not found");
+        }
+    });
 });
