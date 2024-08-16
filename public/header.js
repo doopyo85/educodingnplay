5b3a6d12a37f3549fedd7750bc5b143d0eaaface
doopyo85 <doopyo85@gmail.com>
2024-08-17 05:15:15 +0900

    header.js 정보수정3

--- public/header.js
+++ public/header.js
@@ -1,29 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const userEmailElement = document.getElementById("userEmail");
-    const logoutButtonElement = document.getElementById('logoutButton');
+document.addEventListener("DOMContentLoaded", function () {
+    // 헤더 로드
+    $("#header-placeholder").load("/public/header.html", function () {
+        // 헤더 로드 후에 요소들이 존재하므로 그때 이벤트 핸들러를 등록
+        const userEmailElement = document.getElementById("userEmail");
+        const logoutButtonElement = document.getElementById('logoutButton');
 
-    // userEmail 요소가 존재하는지 확인
-    if (userEmailElement) {
-        fetch('/get-user', { credentials: 'include' })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                userEmailElement.innerText = data.email || "로그인 정보 미확인";
-            })
-            .catch(error => {
-                console.error('Error fetching user data:', error);
-                userEmailElement.innerText = "로그인 정보 미확인";
+        if (userEmailElement) {
+            fetch('/get-user', { credentials: 'include' })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    userEmailElement.innerText = data.email || "로그인 정보 미확인";
+                })
+                .catch(error => {
+                    console.error('Error fetching user data:', error);
+                    userEmailElement.innerText = "로그인 정보 미확인";
+                });
+        }
+
+        if (logoutButtonElement) {
+            logoutButtonElement.addEventListener('click', function () {
+                window.location.href = '/logout';
             });
-    }
-
-    // logoutButton 요소가 존재하는지 확인
-    if (logoutButtonElement) {
-        logoutButtonElement.addEventListener('click', function() {
-            window.location.href = '/logout';
-        });
-    }
+        }
+    });
 });
