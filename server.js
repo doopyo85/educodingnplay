70525546959ccbc9bd5b4089a9e45e04ae502d46
doopyo85 <doopyo85@gmail.com>
2024-08-19 23:32:15 +0900

    scratch-gui 세션연동 server.js수정

--- server.js
+++ server.js
@@ -166,11 +166,13 @@ app.post('/login', (req, res) => {
 
 // 세션 정보 가져오기 API
 app.get('/get-user-session', (req, res) => {
-  console.log('Session in /get-user-session:', req.session);
-  if (req.session && req.session.is_logined) {
-    res.json({ loggedIn: true, username: req.session.nickname });
+  if (req.session && req.session.user) {
+    res.json({ 
+      loggedIn: true, 
+      username: req.session.user.username 
+    });
   } else {
-    res.status(401).json({ loggedIn: false, error: '로그인되지 않은 세션입니다.' });
+    res.json({ loggedIn: false });
   }
 });
 
