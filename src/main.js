c9279c41b426cb5c179404522af68a0e8cff96a3
Ubuntu <ubuntu@ip-172-31-0-184.ap-northeast-2.compute.internal>
2024-09-13 19:20:02 +0000

    webpack

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
-import CodeEditor from 'simple-code-editor/CodeEditor.vue';
+import App from './App.vue';  // App.vue 컴포넌트 가져오기
+import CodeEditor from 'simple-code-editor/CodeEditor.vue';  // Simple Code Editor 컴포넌트 가져오기
 
-const app = createApp({
-  components: {
-    'code-editor': CodeEditor
-  }
-});
+const app = createApp(App);
+
+// Simple Code Editor 컴포넌트 전역 등록
+app.component('code-editor', CodeEditor);
 
 app.mount('#app');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,63 @@
+const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.js', // 메인 진입점
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'public'), // 빌드 파일을 public 폴더에 저장
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.vue$/, // .vue 파일 로더 설정
-        loader: 'vue-loader'
+        test: /\.vue$/,
+        loader: 'vue-loader',
       },
       {
-        test: /\.js$/, // 자바스크립트 파일을 Babel로 처리
+        test: /\.js$/,  // 자바스크립트 파일 처리
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],  // ES6+ 코드를 ES5로 변환
+          },
+        },
       },
       {
-        test: /\.css$/, // CSS 로더 추가
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.ejs$/, // EJS 로더 추가
+        test: /\.ejs$/,
         loader: 'ejs-loader',
         options: {
-          esModule: false
-        }
-      }
-    ]
+          esModule: false,
+        },
+      },
+    ],
   },
   plugins: [
-    new VueLoaderPlugin(), // Vue 로더 플러그인
+    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: './views/index.ejs', // EJS 파일 위치
-      inject: 'body' // 번들된 자바스크립트를 body 끝에 삽입
+      template: './views/index.ejs',
+      inject: 'body',
+    }),
+    new webpack.ProvidePlugin({
+       _: 'lodash'
     })
   ],
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.esm-bundler.js' // Vue 파일 설정
+      vue$: 'vue/dist/vue.esm-bundler.js',
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', '.vue', '.json'],
   },
   devServer: {
     contentBase: './public',
-    hot: true
-  }
+    hot: true,
+  },
 };
 
