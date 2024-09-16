ac00e50a8257812d8ba7fbc5716777296fa247c7
doopyo85 <doopyo85@gmail.com>
2024-09-16 17:46:15 +0900

    server.js수정

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
-require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 const redis = require('redis');
+const db = require('./lib_login/db');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const authRouter = require('./lib_login/auth');
+const { exec } = require('child_process');
+require('dotenv').config();
 const path = require('path');
+const mime = require('mime-types');
+const fs = require('fs');
 const app = express();
 
-// auth.js 라우트 파일 추가
-const authRouter = require('./lib_login/auth');
+// AWS SDK v3 사용
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { fromEnv } = require('@aws-sdk/credential-provider-env');
 
+// S3Client 설정
+const s3Client = new S3Client({
+  region: 'ap-northeast-2',
+  credentials: fromEnv()
+});
+
+// S3에서 객체 가져오는 함수
+const getObjectFromS3 = async (fileName) => {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: fileName
+  };
+
+  try {
+    const data = await s3Client.send(new GetObjectCommand(params));
+    return data.Body;
+  } catch (err) {
+    console.error(`Error fetching file from S3: ${err.message}`);
+    throw err;
+  }
+};
+
+// view 사용 설정
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+// JWT 사용 설정
+
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+
+app.get('/config', (req, res) => {
+  res.json({
+      apiKey: process.env.GOOGLE_API_KEY,
+      discoveryDocs: process.env.DISCOVERY_DOCS,
+      spreadsheetId: process.env.SPREADSHEET_ID,
+  });
+});
 
 const redisClient = redis.createClient({ url: 'redis://localhost:6379' });
 redisClient.connect().catch(console.error);
+
 const store = new RedisStore({ client: redisClient });
 
 app.use(cors({
@@ -29,7 +70,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Content Security Policy 설정
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", 
     "default-src 'self'; " +
@@ -43,6 +83,16 @@ app.use((req, res, next) => {
     "frame-src 'self' https://docs.google.com https://sheets.googleapis.com https://content-sheets.googleapis.com https://educodingnplaycontents.s3.amazonaws.com;"
   );
   next();
+});
+
+app.set('trust proxy', 1);
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
 });
 
 app.use(bodyParser.json());
@@ -64,109 +114,169 @@ app.use(session({
   }
 }));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/resource', express.static(path.join(__dirname, 'public', 'resource')));
-app.use('/node_modules/bootstrap-icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons')));
-app.use('/vue-ide', express.static(path.join(__dirname, 'vue-ide/public')));
-
-app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'resource', 'favicon.ico'));
-});
-
-
-// 인증 미들웨어를 수정
 const authenticateUser = (req, res, next) => {
   const token = req.cookies.token;
 
   if (token) {
     jwt.verify(token, JWT_SECRET, (err, user) => {
       if (err) {
-        console.error('Token verification failed:', err);
-        req.session.is_logined = false;
-        return res.redirect('/auth/login');
+        return res.status(401).json({ loggedIn: false, error: '유효하지 않은 토큰입니다.' });
       }
       req.user = user;
-      req.session.is_logined = true;
       next();
     });
   } else if (req.session && req.session.is_logined) {
     next();
   } else {
-    console.log('User not authenticated, redirecting to login');
-    res.redirect('/auth/login');
+    res.status(401).json({ loggedIn: false, error: '로그인이 필요합니다.' });
   }
 };
 
-// '/get-user-session' 엔드포인트 추가
-app.get('/get-user-session', (req, res) => {
-  if (req.session && req.session.is_logined) {
-    res.json({ username: req.session.username });
-  } else {
-    res.status(401).json({ error: '로그인되지 않은 세션입니다.' });
-  }
-});
-
-// '/get-user' 엔드포인트 추가
-app.get('/get-user', authenticateUser, (req, res) => {
-  res.json({ username: req.session.username });
-});
-
-// auth 라우트 연결
 app.use('/auth', authRouter);
 
-// /config 라우트 - Google API 키와 스프레드시트 ID 등의 정보 제공
-app.get('/config', (req, res) => {
-  res.json({
-    apiKey: process.env.GOOGLE_API_KEY,
-    discoveryDocs: process.env.DISCOVERY_DOCS,
-    spreadsheetId: process.env.SPREADSHEET_ID
+app.use((req, res, next) => {
+  console.log('세션 정보:', req.session);
+  console.log('쿠키 정보:', req.headers.cookie);
+  next();
+});
+
+app.use('/public', (req, res, next) => {
+  const filePath = path.join(__dirname, 'public', req.url);
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      console.error(`File not found: ${filePath}`);
+      return next(); // 파일을 찾지 못한 경우 다음 미들웨어로 넘김
+    }
+
+    console.log(`Serving file: ${filePath}`);
+    
+    // MIME 타입 결정
+    let contentType = mime.lookup(filePath);
+    
+    // JavaScript 파일의 경우 항상 'application/javascript'로 설정
+    if (path.extname(filePath) === '.js') {
+      contentType = 'application/javascript';
+    }
+    
+    // MIME 타입을 결정하지 못한 경우 기본값 사용
+    if (!contentType) {
+      contentType = 'application/octet-stream';
+    }
+
+    res.setHeader('Content-Type', contentType);
+    res.send(content);
   });
 });
 
-// '/test' 엔드포인트 수정
+app.use('/resource', express.static(path.join(__dirname, 'public', 'resource')));
+app.use('/node_modules/bootstrap-icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons')));
+app.use('/vue-ide', express.static(__dirname + '/vue-ide/public'));
+
+app.use((req, res, next) => {
+  const ext = path.extname(req.url).toLowerCase();
+  switch (ext) {
+    case '.js':
+      res.type('application/javascript');
+      break;
+    case '.css':
+      res.type('text/css');
+      break;
+    case '.json':
+      res.type('application/json');
+      break;
+    case '.png':
+      res.type('image/png');
+      break;
+    case '.jpg':
+    case '.jpeg':
+      res.type('image/jpeg');
+      break;
+    case '.wav':
+      res.type('audio/wav');
+      break;
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  if (req.session.is_logined) {
+    res.render('index', { user: req.session.username });
+  } else {
+    res.redirect('/auth/login');
+  }
+});
+
+app.get('/scratch', authenticateUser, (req, res) => {
+  res.render('scratch');
+});
+
+app.get('/computer_basic', authenticateUser, (req, res) => {
+  res.render('computer_basic');
+});
+
+app.get('/entry', authenticateUser, (req, res) => {
+  res.render('entry');
+});
+
+// 라우트 설정
 app.get('/test', authenticateUser, (req, res) => {
   res.render('test', { 
     user: req.session.username,
     googleApiKey: process.env.GOOGLE_API_KEY,
-    spreadsheetId: process.env.SPREADSHEET_ID
+    spreadsheetId: process.env.SPREADSHEET_ID,  // 여기에 쉼표 추가
+    discoveryDocs: process.env.DISCOVERY_DOCS
   });
 });
 
-// Vue.js 라우트에 대한 폴백 처리
-app.get('/vue-ide/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'vue-ide/dist/index.html'));
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
 
-// 에러 핸들링 미들웨어 추가
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+app.get('/get-user', authenticateUser, (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ username: req.session.username });
 });
 
-// 로그인 라우트 수정
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  
-  // 여기에 실제 사용자 인증 로직을 추가해야 합니다.
-  // 예시를 위해 간단한 체크만 수행합니다.
-  if (username === 'test' && password === 'password') {
-    req.session.is_logined = true;
-    req.session.username = username;
+app.get('/scratch-gui', authenticateUser, (req, res) => {
+  const token = req.cookies.token;
+  const scratchGuiUrl = `https://3.34.127.154:8601?token=${token}`;
+  res.redirect(scratchGuiUrl);
+});
 
-    const token = jwt.sign({ username: username, sessionID: req.sessionID }, JWT_SECRET, { expiresIn: '1h' });
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      domain: '.codingnplay.site',
-      maxAge: 3600000
-    });
-
-    console.log('Login successful:', username);
-    res.json({ success: true, username: username });
+app.get('/api/check-login', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  if (req.session && req.session.is_logined) {
+    res.json({ loggedIn: true });
   } else {
-    console.log('Login failed for:', username);
-    res.status(401).json({ success: false, error: '잘못된 사용자명 또는 비밀번호입니다.' });
+    res.json({ loggedIn: false });
+  }
+});
+
+app.post('/login', (req, res) => {
+  const user = { id: 'user-id', username: 'user-name' };
+  
+  req.session.is_logined = true;
+  req.session.username = user.username;
+
+  const token = jwt.sign({ username: user.username, sessionID: req.sessionID }, JWT_SECRET, { expiresIn: '1h' });
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    domain: '.codingnplay.site',
+    maxAge: 3600000
+  });
+
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, username: user.username });
+});
+
+app.get('/get-user-session', (req, res) => {
+  if (req.session && req.session.is_logined) {
+    res.json({ username: req.session.nickname });  // nickname을 username으로 전송
+  } else {
+    res.status(401).json({ error: '로그인되지 않은 세션입니다.' });
   }
 });
 
@@ -180,25 +290,13 @@ app.get('/logout', (req, res) => {
   });
 });
 
-// 라우트 설정
-app.get('/', (req, res) => {
-  if (req.session && req.session.is_logined) {
-    res.render('index', { user: req.session.username });
-  } else {
-    res.redirect('/auth/login');
-  }
-});
-
-// 404 처리
-app.use((req, res, next) => {
-  res.status(404).render('404');
-});
-
+// 이 라우트를 마지막에 배치
 app.get('*', authenticateUser, (req, res) => {
-  res.status(404).render('404', { user: req.session.username });
+  res.render('index');
 });
 
 const DEFAULT_PORT = 3000;
+
 function startServer(port) {
   app.listen(port, (err) => {
     if (err) {
