b2df684e4b986a97eb717ea40c53606192ab9a3e
doopyo85 <doopyo85@gmail.com>
2025-01-17 23:28:43 +0900

    domain add

--- server.js
+++ server.js
@@ -96,14 +96,6 @@ app.use((req, res, next) => {
 
 app.set('trust proxy', 1);
 
-app.use((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
