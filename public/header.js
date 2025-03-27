4944e271f4e847faaa545a0f23453aa3691b1127
doopyo85 <doopyo85@gmail.com>
2025-03-27 13:41:19 +0900

    rerole

--- public/header.js
+++ public/header.js
@@ -1,38 +1,7 @@
 // header.js
 document.addEventListener("DOMContentLoaded", function () {
-    const userNameElement = document.getElementById("userName");
-    
-    if (userNameElement) {
-        fetch('/api/get-user-session', { 
-            credentials: 'include',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.userID) {  // userID로 변경
-                userNameElement.textContent = data.userID;  // userID 표시
-            } else {
-                userNameElement.textContent = '로그인 정보 없음';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching session data:', error);
-            userNameElement.textContent = '로그인 정보 없음';
-        });
-    }
 
-});
-
-// 로그인 폼 제출 이벤트 추가
-document.addEventListener('DOMContentLoaded', function () {
+    // 로그인 폼 제출 이벤트
     const loginForm = document.getElementById('loginForm');
     if (loginForm) {
         loginForm.addEventListener('submit', function (event) {
